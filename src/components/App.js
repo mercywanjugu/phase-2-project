41221249c032797512380152694e1f854d9e86f4
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-07-27 10:23:00 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import '../App.css';
 import Navbar from './Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "./Home";
 
 
 function App() {
-  const randomColor = getRandomColor();
-  const [color, setColor] = useState(randomColor); 
-
-  function handleChangeColor() {
-    const newRandomColor = getRandomColor();
-    setColor(newRandomColor); 
+  
+  // const randomColor = getRandomColor();
+  const [colors, setColors] = useState([]);
 
   useEffect(() => {
     fetch("https://calm-falls-89460.herokuapp.com/colors")
       .then((response) => response.json())
       .then((data) => {
-        setColor(data.color);
+        console.log(data[0].hex);
       });
   }, []);
-  return (<Navbar/>)
- 
-  //   <div className="App">
-//       <header className="ECDE-COLOR-APP">
-//         { <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn Colors
-//         </a> }
-//       </header>
-//     </div>
-//   );
- }
+
+  return (<Fragment>
+    <Navbar/>
+    
+    <ul>
+      {
+        colors.map((color, index) => {
+          return <li key={index} index>{color?.name}</li>
+         })
+      }
+    </ul>
+    <Home/>
+  </Fragment>)
+
+}
 
 export default App;
