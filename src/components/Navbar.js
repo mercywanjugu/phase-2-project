6b56c4f6e010b51acecc9e276e3cbe1496d2a54f
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-07-31 20:09:27 +0300

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 
 function Navbar(){
   return <nav className="navbar navbar-default">
 <div className="container-fluid">
   <div className="navbar-header">
-    <a className="navbar-brand" href="#">ECDE LEARNING APP</a>
+    <a className="navbar-brand" href="/">ECDE COLOR APP</a>
   </div>
   <ul className="nav navbar-nav">
-    {/* <li className="active"><a href="#">Home</a></li> */}
-    <Link to="/">Home</Link>
-    <Link to="/contact">Contact</Link>
-    <Link to="/about">About</Link>
+    { <li className="active"><a href="/">Home</a></li> }
+    {/* <Link to="/">Home</Link> */}
+    {/* <Link to="/contact">Contact</Link>
+    <Link to="/about">About</Link> */}
 
 
 
-    {/* <li><a href="#">Contacts</a></li>
-    <li><a href="#">About</a></li> */}
+     <li><a href="/Contact">Contact</a></li>
+    <li><a href="/About">About</a></li> 
   </ul>
 </div>
 </nav>
