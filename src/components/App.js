f6800e359e0bce8c2541b7ed08e7feacd422ccdb
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-07-31 20:07:06 +0300

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment } from "react";
+  import React from "react";
 // import '../App.css';
 import Navbar from './Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,7 +9,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   
-
   return (
     <Router>
     <div className="App">
@@ -22,29 +21,13 @@ function App() {
           <Route exact path="/about">
             <About/>
           </Route>
-          <Route path="Contact">
+          <Route path="/Contact">
             <Contact/>
           </Route>
         </Switch>
       </div>
     </div>
   </Router>
-    // <Router>
-    //   <div>
-    //     <Navbar/>
-    //     <switch>
-    //       <Route path="/">
-    //     <Home/>
-    //       </Route>
-    //       <Route exact path="/">
-    //     <Contact/>
-    //       </Route>
-    //       <Route path="/">
-    //     <About/>
-    //       </Route>
-    //     </switch>
-    //   </div>
-    // </Router>
  )
 
 }
