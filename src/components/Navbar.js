2de7bfe44d600c68f01eadb436f1aef9b5142946
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-08-01 10:48:45 +0300

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,14 +10,14 @@ function Navbar(){
   </div>
   <ul className="nav navbar-nav">
     { <li className="active"><a href="/">Home</a></li> }
-    {/* <Link to="/">Home</Link> */}
-    {/* <Link to="/contact">Contact</Link>
-    <Link to="/about">About</Link> */}
+    <Link to="/">Home</Link> 
+    <Link to="/contact">Contact</Link>
+    <Link to="/about">About</Link> 
 
 
 
-     <li><a href="/Contact">Contact</a></li>
-    <li><a href="/About">About</a></li> 
+     {/* <li><a href="/Contact">Contact</a></li>
+    <li><a href="/About">About</a></li>  */}
   </ul>
 </div>
 </nav>
